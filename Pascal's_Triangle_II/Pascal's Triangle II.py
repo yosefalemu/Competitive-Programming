16b71b11edd2a0ff7c95f2c733aab06e86b00886
yosefalemu <125744573+yosefalemu@users.noreply.github.com>
2023-02-27 00:30:53 +0300

    Create Pascal's Triangle II.py

---
+++ Pascal's_Triangle_II/Pascal's Triangle II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        ans = [1]
+        if rowIndex == 0:
+            return ans
+        else:
+            for i in range(rowIndex):
+                temp = []
+                temp = [0] + ans + [0]
+                ans = []
+                for j in range(len(temp) - 1):
+                    ans.append(temp[j] + temp[j + 1])
+        return ans
