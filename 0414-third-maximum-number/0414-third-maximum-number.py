9e5f1f4f6dba6a31d3a7d4a32b87e08ed44c7b16
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-02 23:01:00 +0300

    Time: 59 ms (44.14%), Space: 14.9 MB (76.45%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,14 +1,17 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        temp = set(nums)
-        ans = []
-        for c in temp:
-            ans.append(c)
-        ans.sort(reverse = True)
-        n = len(ans)
-        if n >= 3:
-            return ans[2]
-        else:
-            return ans[0]
+        nums.sort(reverse = True)
+        count = 1
+        slowpt = 0
+        for fastpt in range(1, len(nums)):
+            if nums[slowpt] != nums[fastpt]:
+                slowpt = fastpt
+                count += 1
+            if count == 3:
+                return nums[slowpt]
+        return nums[0]
+                
+            
+            
             
         
