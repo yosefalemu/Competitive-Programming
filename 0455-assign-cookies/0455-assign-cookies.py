deb888e6f297000a61c6ec015d5cc4f4d6ea54b9
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-03 11:30:50 +0300

    Time: 173 ms (52.53%), Space: 15.7 MB (75.12%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        leftpt, rightpt,count = 0, 0, 0
+        while leftpt < len(g) and rightpt < len(s):
+            if s[rightpt] >= g[leftpt]:
+                count += 1
+                leftpt += 1
+            rightpt += 1
+        return count
+        
