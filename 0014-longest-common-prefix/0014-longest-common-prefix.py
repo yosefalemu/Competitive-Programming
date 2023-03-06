c1dd7268acd1dc2162f53dbc7a54468a9269d8cf
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-06 22:27:57 +0300

    Time: 36 ms (64.42%), Space: 14 MB (6.63%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,7 +1,5 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        standard = strs[0]
-        ans = ""
         dict1 = {}
         for c in strs:
             if c in dict1:
@@ -9,6 +7,8 @@ class Solution:
             else:
                 dict1.update({c:len(c)})
         length = min(dict1.values())
+        standard = strs[0]
+        ans = ""
         for i in range(length):
             for j in range(len(strs)):
                 if strs[j][i] == standard[i]:
