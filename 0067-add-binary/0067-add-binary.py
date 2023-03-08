8f6abf07732acea9aa17c643986ebba2cc16cb93
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-08 10:06:43 +0300

    Time: 29 ms (90.50%), Space: 13.9 MB (62.60%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,33 +1,22 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         carry = 0
-        ans = ""
-        i = 0
-        j = 0
-        a = a[::-1]
-        b = b[::-1]
-        while i < len(a) or j < len(b):
-            if i < len(a) and j < len(b):
-                digitA = int(a[i])
-                digitB = int(b[j])
-            elif i < len(a) and j >= len(b):
-                digitA = int(a[i])
-                digitB = 0
-            elif i >= len(a) and j < len(b):
-                digitA = 0
-                digitB = int(b[j])
-            else:
-                pass
-            i += 1
-            j += 1
-            total = digitA + digitB + carry
-            sum1 = total % 2
+        ans = []
+        i = len(a) - 1
+        j = len(b) - 1
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            temp = total % 2
             carry = total // 2
-            ans += str(sum1)
-        if carry:
-            return "1" + ans[::-1]
-        else:
-            return ans[::-1]
+            ans.append(str(temp))
+        return "".join(reversed(ans))
+                
             
             
             
