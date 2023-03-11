e4b74913bcb46c56d37cd36d31a4677a45dd45b7
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-11 22:35:46 +0300

    Time: 38 ms (16.02%), Space: 13.8 MB (89.68%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
@@ -1,5 +1,9 @@
 class Solution:
     def countSegments(self, s: str) -> int:
-        temp = s.split()
-        return len(temp)
+        count = 0
+        for i in range(len(s)):
+            if (i == 0 or s[i - 1] == " ") and s[i] != " ":
+                count += 1
+        return count
+        
         
