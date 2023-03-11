7ea1a2e87f1c98ebe2c968e983e85d716af9600c
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-11 19:50:19 +0300

    Time: 53 ms (74.30%), Space: 15 MB (79.33%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,12 +1,8 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        s.lower()
         dict1 = {'a':1, 'e':1, 'i':1, 'o':1, 'u':1, 'A':1, 'E':1, 'I':1, 'O':1, 'U':1}
+        tempstr = list(s)
         leftpt, rigthpt = 0, len(s) - 1
-        tempstr = []
-        ans = ""
-        for c in s:
-            tempstr.append(c)
         while leftpt < rigthpt:
             if tempstr[leftpt] in dict1 and tempstr[rigthpt] in dict1:
                 tempstr[leftpt], tempstr[rigthpt] = tempstr[rigthpt], tempstr[leftpt]
@@ -19,7 +15,4 @@ class Solution:
             else:
                 leftpt += 1
                 rigthpt -= 1
-        for c in tempstr:
-            ans += c
-        return ans
-        
+        return "".join(tempstr)
