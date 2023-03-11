37377705b4e2b1b36713dd87c4595b67099af3cd
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-11 20:45:57 +0300

    Time: 56 ms (63.50%), Space: 14.1 MB (89.59%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         counter1, counter2 = Counter(ransomNote), Counter(magazine)
-        if counter1 & counter2 == counter1:
-            return True
-        else:
-            return False
+        return not counter1 - counter2
             
             
         
