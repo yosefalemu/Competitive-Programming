6b0170642e932aa94894faf8e3c9971388089ac0
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-11 21:30:13 +0300

    Time: 37 ms (41.18%), Space: 13.8 MB (98.22%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        leftpt, rigthpt = 0, 0
+        while rigthpt < len(t) and leftpt < len(s):
+            if s[leftpt] == t[rigthpt]:
+                leftpt += 1
+                rigthpt += 1
+            else:
+                rigthpt += 1
+        if leftpt == len(s):
+            return True
+        else:
+            return False
+        
