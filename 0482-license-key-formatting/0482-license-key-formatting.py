384acf0a284c0c6a183c9c7600c9522ae8606247
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-03-12 12:07:36 +0300

    Time: 36 ms (94.86%), Space: 14.2 MB (96.95%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.py
+++ 0482-license-key-formatting/0482-license-key-formatting.py
@@ -1,19 +1,15 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        i = len(s) - 1
-        ans = ""
-        count = 0
-        while i >= 0:
-            if count < k and s[i] != "-":
-                ans = s[i].upper() + ans
-                count += 1
-                i -= 1
-            elif count == k:
-                ans = "-" + ans
-                count = 0
-            else:
-                i -= 1
-        if (len(ans) > 0 and ans[0] == '-'):
-            ans = ans[1:]
-        return ans
+        s = s.replace("-", "").upper()
+        intial_length = len(s) % k
+        group_text = s[:intial_length]
+        for i in range(intial_length,len(s),k):
+            if  group_text:
+                group_text += "-"
+            group_text += s[i: i + k]
+        return group_text
+                
+            
+        
+                
             
