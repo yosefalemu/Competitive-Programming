8d7722eb923dcd290edd364fd4796a0ae916a2c9
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-04-04 12:25:17 +0300

    Time: 13 ms (42.33%), Space: 11.2 MB (5.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow == fast){
+        unordered_map<ListNode*, int>dict;
+        while(head != NULL){
+            if(dict[head] == 1){
                 return true;
             }
+            else{
+                dict[head] = 1;
+            }
+            head = head->next;
         }
         return false;
     }
