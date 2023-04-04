83588756f6e49e2504241eaf5480ce2614574c17
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-04-04 12:13:58 +0300

    Time: 12 ms (96.38%), Space: 19.8 MB (5.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,8 +3,16 @@ public:
     int majorityElement(vector<int>& nums) {
         int length_nums = nums.size();
         int middle_length_nums = nums.size()/2;
-        
-        sort(nums.begin(),nums.end());
-        return nums[middle_length_nums];
+        unordered_map<int, int>dict1;
+        for(int i = 0; i < nums.size();i++){
+            dict1[nums[i]] ++;
+        }
+        int majority_ele = 0;
+        for(auto item:dict1){
+            if(item.second > middle_length_nums){
+                majority_ele = item.first;
+            }
+        }
+        return majority_ele;
     }
 };
