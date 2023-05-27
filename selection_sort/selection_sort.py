9e1cd95bc0a726dcb95b1731ecec746641bf9fe9
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-05-27 23:51:17 +0300

    Create selection_sort.py

---
+++ selection_sort/selection_sort.py
@@ -0,0 +1,14 @@
+class Solution: 
+    def select(self, input_list, i):
+        n = len(input_list)
+        for i in range(n - 1):
+            min_index = i
+            for j in range(i, n):
+                if input_list[j] < input_list[min_index]:
+                    min_index = j
+            if min_index != i:
+                input_list[i], input_list[min_index] = input_list[min_index], input_list[i]
+        return input_list
+    
+    def selectionSort(self, arr,n):
+        return self.select(arr, 0)
