ab0084c1615fdaff3b24cd87fe78aa1a19e4a594
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-07-02 21:05:56 +0300

    Time: 96 ms (80.55%), Space: 18 MB (59.91%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,16 +3,14 @@ class Solution:
         length = len(nums)
         slowpt = 0
         fastpt = 1
-        count = 1
-        index = 0
+        index = 1
         while fastpt < length:
             if nums[slowpt] != nums[fastpt]:
-                nums[index + 1] = nums[fastpt]
+                nums[index] = nums[fastpt]
                 index += 1
-                count += 1
                 slowpt = fastpt
             fastpt += 1
-        return count
+        return index
        
             
             
