ee4be06bc03eaa092f0927e713adb0871b2486a7
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-07-07 00:03:36 +0300

    Time: 70 ms (89.24%), Space: 16.4 MB (54.07%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,17 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        leftpt = rightpt = i = 0
+        n = len(chars)
+        while leftpt < n:
+            while rightpt < n and chars[rightpt] == chars[leftpt]:
+                rightpt += 1
+            chars[i] = chars[leftpt]
+            i += 1
+            if rightpt - leftpt > 1:
+                for c in str(rightpt -leftpt):
+                    chars[i] = c
+                    i += 1
+            leftpt = rightpt
+        return i
+        
+        
