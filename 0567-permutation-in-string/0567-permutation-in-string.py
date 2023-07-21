17881d283aff0e774849486981ed5df8bd5bb213
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-07-21 12:06:53 +0300

    Time: 71 ms (95.00%), Space: 16.6 MB (5.01%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1Count, s2Count = {}, {}
+        if len(s1) > len(s2):
+            return False
+        for i in range(len(s1)):
+            if s1[i] in s1Count:
+                s1Count[s1[i]] += 1
+            else:
+                s1Count[s1[i]] = 1
+            if s2[i] in s2Count:
+                s2Count[s2[i]] += 1
+            else:
+                s2Count[s2[i]] = 1
+        if s1Count == s2Count:
+            return True
+        l = 0
+        for i in range(len(s1), len(s2)):
+            if s2[i] in s2Count:
+                s2Count[s2[i]] += 1
+            else:
+                s2Count[s2[i]] = 1
+            s2Count[s2[i - len(s1)]] -= 1
+            if s2Count[s2[i - len(s1)]] == 0:
+                del s2Count[s2[i - len(s1)]]
+            if s1Count == s2Count:
+                return True
+        return False
