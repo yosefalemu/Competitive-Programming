e428885dbb9ada66a0b6e9c9463bcad6fe97eba1
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2023-07-21 13:06:22 +0300

    Update Number-of-smaller.py

--- Number-of-smaller/Number-of-smaller.py
+++ Number-of-smaller/Number-of-smaller.py
@@ -1,18 +1,20 @@
-n,m = map(int,input().split())
-ns  =  list(map(int,input().split()))
-ms = list(map(int,input().split()))
- 
-i,j = 0,0
+n, m = map(int, input().split())
+arr_n = list(map(int, input().split()))
+arr_m = list(map(int, input().split()))
+
+i, j = 0, 0
 last_printed = 0
-while i < n and j< m:
-    while i < n and ns[i] < ms[j]:
+
+while i < n and j < m:
+    while i < n and arr_n[i] < arr_m[j]:
         i += 1
     else:
         i -= 1
-    
-    print(i+1,end = " ")
-    j +=1 
-    i +=1
+
+    print(i + 1, end=" ")
+    j += 1
+    i += 1
+
 while j < m:
-    print(i,end = " ")
-    j+=1
+    print(i, end=" ")
+    j += 1
